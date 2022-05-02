b37a8b1f2d91a9b068b1666c740c8883371e687a
Richard White <rwhite@crashwhite.com>
2022-05-02 12:13:23 -0700

    First commit--my game works

---
+++ game.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+game.py
+"""
+
+import random
+
+def main():
+    n = int(random.randrange(3) + 1)
+    g = int(input("Guess a number 1-3: "))
+    if n == g:
+        print("You guessed it!")
+    else:
+        print("Sorry, the number was " + str(n))
+    
+if __name__ == "__main__":
+    main()
